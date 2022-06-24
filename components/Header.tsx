c09ce15db35dfd9eeec831b7234668c48dd9edaa
GnaniPasupula7 <gnani.p@admybrand.com>
2022-06-24 22:08:27 +0530

    Finished header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,28 +1,34 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function Header() {
   return (
-    <header className='flex justify-between p-5 max-w-7xl mx-auto'>
-        <div className='flex items-center space-x-5'>
-            <Link href="/">
-                <img className='w-44 object-contain cursor-pointer' src="https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png" alt=""/>
-            </Link>
-            <div className='hidden md:inline-flex items-center space-x-5'>
-                <h3>About</h3>
-                <h3>Contact</h3>
-                <h3 className='text-white bg-green-600 px-4 py-1 rounded-full'>Follow</h3>
-            </div>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <img
+            className="w-44 object-contain cursor-pointer"
+            src="https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png"
+            alt=""
+          />
+        </Link>
+        <div className="hidden md:inline-flex items-center space-x-5">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
+            Follow
+          </h3>
         </div>
+      </div>
 
-        <div className='flex items-center text-green-600 space-x-5'>
-            <h3>Sign in</h3>
-            <h3 className='px-4 py-1 border border-green-600 rounded-full'>Get Started</h3>
-        </div>
-        
+      <div className="flex items-center text-green-600 space-x-5">
+        <h3>Sign in</h3>
+        <h3 className="px-4 py-1 border border-green-600 rounded-full">
+          Get Started
+        </h3>
+      </div>
     </header>
-     
-  )
+  );
 }
 
-export default Header
+export default Header;
